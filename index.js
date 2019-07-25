635f9538c49db8c48967c696784e93dd4cbf54dc
Hikaru Sato <hisato@solxyz.co.jp>
2019-07-25 12:51:14 +0900

    settings for heroku

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 var express = require('express')
 
+var app = express()
+
+app.use(express.static('public'))
+
+var port = process.env.PORT || 3000
+
+app.listen(port,function(){
+    console.log("Expressサーバーがポート%dで起動しました。モード:%s",port,app.settings.env)
+})
